e8f4879fb602e9f014d60d2867e12f1a19ef4cb4
woodong27 <cdw5548@gmail.com>
2023-02-08 22:34:22 +0900

    코드 수정

--- Feb.2nd_week/1966(숫자를 정렬하자).py
+++ Feb.2nd_week/1966(숫자를 정렬하자).py
@@ -2,5 +2,13 @@ T=int(input())
 for x in range(T):
     N=int(input())
     lst=list(map(int,input().split()))
-    lst.sort()
+
+    #lst.sort()
+
+    #sort직접 구현하기
+    for i in range(len(lst)):
+        for j in range(i+1,len(lst)):
+            if lst[i]>=lst[j]:
+                lst[i],lst[j]=lst[j],lst[i]
+
     print(f'#{x+1}',*lst)
