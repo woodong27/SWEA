0fa8c73e3143fe76b8ee0414462e0e85caaab2d6
woodong27 <cdw5548@gmail.com>
2023-02-16 15:30:04 +0900

    02/16/2023

--- Feb.3rd_week/16583(토너먼트 카드게임).py
+++ Feb.3rd_week/16583(토너먼트 카드게임).py
@@ -1,21 +1,39 @@
 T=int(input())
 
-def divide(lst):
-    if len(lst)==1:
-        return lst
+def game(lst):
+    n=len(lst)
+    if n==1:
+        return lst[0]
 
-def winner(lst):
-    if abs(lst[0]-lst[1])==2:
-        return min(lst)
+    elif n==2:
+        if lst[0][1]-lst[1][1]==2:
+            return lst[1]
+        elif lst[0][1]-lst[1][1]==-2:
+            return lst[0]
+        elif lst[0][1]==lst[1][1]:
+            return lst[0]
+        elif lst[0][1]-lst[1][1]==1:
+            return lst[0]
+        elif lst[0][1]-lst[1][1]==-1:
+            return lst[1]
 
-    elif abs(lst[0]-lst[1])==1:
-        return max(lst)
+    else:
+        if n%2:
+            l1=game(lst[0:n//2+1])
+            l2=game(lst[n//2+1:n])
+            return game([l1,l2])
 
-
-
+        else:
+            l1=game(lst[0:n//2])
+            l2=game(lst[n//2:n])
+            return game([l1,l2])
 
 for x in range(T):
     N=int(input())
     cards=list(map(int,input().split()))
 
+    new=[]
+    for i in enumerate(cards):
+        new.append(i)
 
+    print(f'#{x+1} {game(new)[0]+1}')
