24946b3d5e1ffca6e885608d0f9fdf7dba4bb2d5
woodong27 <cdw5548@gmail.com>
2023-02-17 11:29:17 +0900

    02/17/2023

---
+++ Feb.3rd_week/10760(우주선착륙2).py
@@ -0,0 +1,21 @@
+T=int(input())
+
+di=[-1,1,0,0,-1,-1,1,1]
+dj=[0,0,-1,1,-1,1,1,-1] #상,하,좌,우,11시,1시,5시,7시
+
+for tc in range(T):
+    N,M=map(int,input().split())
+    pic=[list(map(int,input().split()))for _ in range(N)]
+
+    ans=0
+    for i in range(0,N):
+        for j in range(M):
+            stack=[]
+            for k in range(8):
+                if 0<=i+di[k]<N and 0<=j+dj[k]<M and pic[i+di[k]][j+dj[k]]<pic[i][j]:
+                    stack.append(pic[i+di[k]][j+dj[k]])
+
+            if len(stack)>=4:
+                ans+=1
+
+    print(f'#{tc+1} {ans}')
