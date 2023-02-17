f2a2b559a44a2ce130d8a88221be5c7735102eeb
woodong27 <cdw5548@gmail.com>
2023-02-17 14:52:47 +0900

    02/17/2023

--- Feb.3rd_week/10760(우주선착륙2).py
+++ Feb.3rd_week/10760(우주선착륙2).py
@@ -8,13 +8,13 @@ for tc in range(T):
     pic=[list(map(int,input().split()))for _ in range(N)]
 
     ans=0
-    for i in range(0,N):
+    for i in range(N):
         for j in range(M):
             stack=[]
             for k in range(8):
                 if 0<=i+di[k]<N and 0<=j+dj[k]<M and pic[i+di[k]][j+dj[k]]<pic[i][j]:
                     stack.append(pic[i+di[k]][j+dj[k]])
-
+            print(i,j,stack)
             if len(stack)>=4:
                 ans+=1
 
