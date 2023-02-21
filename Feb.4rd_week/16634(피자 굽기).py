209d28ea3c94da4e97a31e4691401f8b634fbe7c
woodong27 <cdw5548@gmail.com>
2023-02-21 11:24:55 +0900

    코드 수정(deque모듈 사용)

--- Feb.4rd_week/16634(피자 굽기).py
+++ Feb.4rd_week/16634(피자 굽기).py
@@ -1,25 +1,27 @@
+from collections import deque
+
 T=int(input())
 
 for tc in range(T):
     N,M=map(int,input().split())
     lst=list(map(int,input().split()))
 
-    temp=[]
+    pizzas=deque([])
     for idx,cheese in enumerate(lst):
-        temp.append((idx+1,cheese))
+        pizzas.append((idx+1,cheese))
 
-    pizza=[]
+    oven=deque([])
     for i in range(N):
-        pizza.append(temp.pop(0))
+        oven.append(pizzas.popleft())
 
-    while pizza:
-        idx,cheese=pizza.pop(0)
+    while oven:
+        idx,cheese=oven.popleft()
         cheese//=2
         if cheese!=0:
-            pizza.append((idx,cheese))
-        elif temp:
-            pizza.append(temp.pop(0))
-        else:
+            oven.append((idx,cheese))
+        elif pizzas:
+            oven.append(pizzas.popleft())
+        elif not oven:
             ans=idx
 
     print(f'#{tc+1} {ans}')
