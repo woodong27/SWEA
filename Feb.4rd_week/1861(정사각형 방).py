bc9b572ee5fa7ec55f622c8a49ffea2263e276b9
woodong27 <cdw5548@gmail.com>
2023-02-25 16:49:32 +0900

    02/25/2023

--- Feb.4rd_week/1861(정사각형 방).py
+++ Feb.4rd_week/1861(정사각형 방).py
@@ -6,41 +6,33 @@ di=[-1,1,0,0]
 dj=[0,0,-1,1]
 
 def bfs(si,sj):
-    visited=[[0 for _ in range(N)]for _ in range(N)]
-    q=deque()
-    q.append((si,sj,1))
+    q=deque([(si,sj)])
     visited[si][sj]=1
-    distances=[]
+    path=[rooms[si][sj]]
     while q:
-        ci,cj,cnt=q.popleft()
-        distances.append((rooms[si][sj],cnt))
+        ci,cj=q.popleft()
         for k in range(4):
             ni,nj=ci+di[k],cj+dj[k]
-            if 0<=ni<N and 0<=nj<N and rooms[ni][nj]-rooms[ci][cj]==1:
+            if 0<=ni<N and 0<=nj<N and abs(rooms[ni][nj]-rooms[ci][cj])==1:
                 if not visited[ni][nj]:
-                    q.append((ni,nj,cnt+1))
+                    q.append((ni,nj))
                     visited[ni][nj]=1
+                    path.append(rooms[ni][nj])
 
-    return distances
-
-import time
+    return min(path),len(path)
 
 for tc in range(T):
-    start=time.time()
     N=int(input())
     rooms=[list(map(int,input().split()))for _ in range(N)]
+    visited = [[0 for _ in range(N)] for _ in range(N)]
 
-    resultd,resultv=0,N**2
-    result=0
+    ansv,ansd=N**2, 0
     for i in range(N):
         for j in range(N):
-            if not result:
-                result=bfs(i,j)
-            elif len(bfs(i,j))>len(result):
-                result=bfs(i,j)
-            elif len(bfs(i,j))==len(result):
-                if result[0][0]>bfs(i,j)[0][0]:
-                    result=bfs(i,j)
+            if not visited[i][j]:
+                resultv,resultd=bfs(i,j)
+                if resultd>ansd or resultd==ansd and resultv<ansv:
+                    ansv=resultv
+                    ansd=resultd
 
-    end=time.time()
-    print(f'#{tc+1} {result[-1][0]} {result[-1][1]} // 실행시간 : {end-start}초')
+    print(f'#{tc+1}',ansv,ansd)
