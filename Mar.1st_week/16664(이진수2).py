074286200cdf72bcab0ff32ee6ce3a4e69781a69
woodong27 <cdw5548@gmail.com>
2023-02-25 20:38:28 +0900

    02/25/2023

---
+++ Mar.1st_week/16664(이진수2).py
@@ -0,0 +1,29 @@
+T=int(input())
+
+for tc in range(T):
+    num=float(input())
+
+    cnt=0
+    ans=[]
+    flag=0
+    while num!=1:
+        cnt+=1
+        if num*2>1:
+            ans.append(1)
+        elif num*2<1:
+            ans.append(0)
+
+        num*=2
+        if num>1:
+            num-=1
+
+        if cnt>13:
+            flag=1
+            break
+
+    print(f'#{tc+1}',end=' ')
+    if flag:
+        print('overflow')
+    else:
+        ans.append(1)
+        print(*ans,sep='')
