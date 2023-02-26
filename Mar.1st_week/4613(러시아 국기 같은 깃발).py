d1973f2f75f6ce3c8110368d6583583869d2325f
woodong27 <cdw5548@gmail.com>
2023-02-26 20:19:18 +0900

    러시아 국기 미완성

---
+++ Mar.1st_week/4613(러시아 국기 같은 깃발).py
@@ -0,0 +1,21 @@
+T=int(input())
+
+from pprint import pprint
+
+for tc in range(T):
+    N,M=map(int,input().split())
+    flag=[list(input())for _ in range(N)]
+
+    # pprint(flag)
+    ans=0
+    for j in range(M):
+        if flag[0][j]!='W':
+            ans+=1
+        if flag[N-1][j]!='R':
+            ans+=1
+
+    for i in range(1,N-1):
+        if flag[i].count('W')>=flag[i].count('B'):
+
+
+    print(ans)
