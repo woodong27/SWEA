8f6f23cad289fdc8365c6788cca0c64493c89dd4
woodong27 <cdw5548@gmail.com>
2023-02-27 17:01:14 +0900

    02/27/2023 러시아 국기 못풀었음

--- Mar.1st_week/4613(러시아 국기 같은 깃발).py
+++ Mar.1st_week/4613(러시아 국기 같은 깃발).py
@@ -5,17 +5,13 @@ from pprint import pprint
 for tc in range(T):
     N,M=map(int,input().split())
     flag=[list(input())for _ in range(N)]
+    pprint(flag)
 
-    # pprint(flag)
     ans=0
     for j in range(M):
         if flag[0][j]!='W':
             ans+=1
         if flag[N-1][j]!='R':
             ans+=1
-
-    # for i in range(1,N-1):
-    #     if flag[i].count('W')>=flag[i].count('B'):
-
 
     print(ans)
