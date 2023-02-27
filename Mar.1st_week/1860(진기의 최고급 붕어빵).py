379e9c140d739a6497f6121d31563533fd7c8dec
woodong27 <cdw5548@gmail.com>
2023-02-27 15:32:05 +0900

    02/27/2023

--- Mar.1st_week/1860(진기의 최고급 붕어빵).py
+++ Mar.1st_week/1860(진기의 최고급 붕어빵).py
@@ -3,8 +3,28 @@ T=int(input())
 for tc in range(T):
     N,M,K=map(int,input().split())
 
-    people=list(map(int,input().split()))
+    arrivetime=list(map(int,input().split()))
 
-    bread=M*K
-    print(len(people),N)
-    print(bread)
+    people=[0 for _ in range(max(arrivetime)+1)]
+    for i in arrivetime:
+        people[i]+=1
+
+    ans='Possible'
+    if min(arrivetime)<M:
+        ans='Impossible'
+    else:
+        seconds=1
+        bread=0
+        while seconds<=max(arrivetime):
+            if not seconds%M:
+                bread+=K
+
+            bread-=people[seconds]
+
+            if bread<0:
+                ans='Impossible'
+                break
+
+            seconds+=1
+
+    print(f'#{tc+1} {ans}')
