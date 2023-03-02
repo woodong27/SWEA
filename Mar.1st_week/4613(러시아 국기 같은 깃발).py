6abc136d028aa79ea7ca7592677387f1a90fa057
woodong27 <cdw5548@gmail.com>
2023-03-02 22:22:31 +0900

    03/02/2023

--- Mar.1st_week/4613(러시아 국기 같은 깃발).py
+++ Mar.1st_week/4613(러시아 국기 같은 깃발).py
@@ -1,17 +1,29 @@
 T=int(input())
 
-from pprint import pprint
-
 for tc in range(T):
     N,M=map(int,input().split())
     flag=[list(input())for _ in range(N)]
-    pprint(flag)
 
-    ans=0
-    for j in range(M):
-        if flag[0][j]!='W':
-            ans+=1
-        if flag[N-1][j]!='R':
-            ans+=1
+    minv=N*M
+    for i in range(0,N-2):
+        for j in range(1,N-1-i):
+            cnt=0
+            for ii in range(i+1):
+                for jj in range(M):
+                    if flag[ii][jj]!='W':
+                        cnt+=1
 
-    print(ans)
+            for ii in range(i+1,i+j+1):
+                for jj in range(M):
+                    if flag[ii][jj]!='B':
+                        cnt+=1
+
+            for ii in range(i+j+1,N):
+                for jj in range(M):
+                    if flag[ii][jj]!='R':
+                        cnt+=1
+
+            if cnt<minv:
+                minv=cnt
+
+    print(f'#{tc+1} {minv}')
